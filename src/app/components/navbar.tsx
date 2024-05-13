ca801ca0c8458a7d0bf3a98f8a51fa734db8d07b
goncalos10 <a18847@alunos.ipca.pt>
2024-05-13 21:39:14 +0100

    Added navbar to project

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+            <a className="navbar-brand" href="#">
+                <Image src="./logotipo.svg" alt="logo" width={100} height={50} />
+            </a>
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <Link className="nav-link" href="/vehicles">
+                        Vehicles
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" href="/users">
+                        Users
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" href="/testdrives">
+                        Test Drives
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 'use client'
 import { useEffect } from "react";
-
+import Navbar from "./components/navbar";
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
     import("bootstrap/dist/js/bootstrap");
   }, []);
-  return <h1>Hello</h1>
+  return <>
+    <Navbar />
+  </>
 }
 export default MyApp;
