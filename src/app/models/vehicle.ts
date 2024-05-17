6f55ff49f8322128a15bd25f1899d3226b175215
goncalos10 <a18847@alunos.ipca.pt>
2024-05-17 20:15:06 +0100

    Updated vehicles model

--- src/app/models/vehicle.ts
+++ src/app/models/vehicle.ts
@@ -1,7 +1,10 @@
 export interface VehicleModel {
-    id: number;
-    standid: number;
-    brandname: string;
+    id?: number;
+    vehicleid?: number;
+    standid?: number;
+    brandname?: string;
+    brandid?: number;
+    gastypename?: number;
     gastypeid: number;
     model: string;
     year: number;
